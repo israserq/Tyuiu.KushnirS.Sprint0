25ca87485fc85f3c295378d41ed640f93ad93685
User <User@DESKTOP-8Q26HPT>
2024-09-20 00:35:10 +0500

    Add project files.

---
+++ Tyuiu.KushnirS.Sprint0.task0.V00/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KushnirS.Sprint0.task0.V00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Tyuiu.KushnirS.Sprint0.task0.v0/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Tyuiu.KushnirS.Sprint0.task0.v0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
