5f4fd1b092d29e31a3cbfed5142c314bfd9697b0
User <User@DESKTOP-8Q26HPT>
2024-09-20 12:22:33 +0500

    task1

---
+++ tyuiu.KushnirS.sprint0.task1.v0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace tyuiu.KushnirS.sprint0.task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            string name = "серега";
+            int age = 18;
+            Console.WriteLine("Приыеь меня зовут "+name+" мне "+age+" лет)");
+        }
+    }
+}
